c339ec08b6875693b284de60924417423b115230
Ahmed Elzoughby <gs_elzoughby@yahoo.com>
2019-02-28 21:42:54 +0200

    fix the https handshaking error and the file not exist error when deleting item

--- packager/scripts/build-package.bat
+++ packager/scripts/build-package.bat
@@ -14,12 +14,12 @@ call "%JAVA_HOME%\bin\java.exe" ^
     --module-path "%MODULE_PATH%" ^
     --verbose ^
     --echo-mode ^
-    --add-modules "java.base,java.datatransfer,java.desktop,java.scripting,java.xml,jdk.jsobject,jdk.unsupported,jdk.unsupported.desktop,jdk.xml.dom,javafx.controls,javafx.fxml,java.naming,java.sql,jdk.charsets" ^
+    --add-modules "java.base,java.datatransfer,java.desktop,java.instrument,java.logging,java.management,java.scripting,java.sql,java.xml,jdk.attach,jdk.jdi,jdk.jsobject,jdk.unsupported,jdk.unsupported.desktop,jdk.xml.dom,javafx.controls,javafx.graphics,javafx.fxml,java.naming,jdk.charsets" ^
     --input "%INPUT%" ^
     --output "%OUTPUT%" ^
     --name "VDM" ^
     --main-jar "%JAR%" ^
     --version "%VERSION%" ^
-    --jvm-args "--add-opens javafx.base/com.sun.javafx.reflect=ALL-UNNAMED" ^
+    --jvm-args "-Djdk.tls.client.protocols=TLSv1.2 --add-opens javafx.base/com.sun.javafx.reflect=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens javafx.graphics/com.sun.javafx.tk=ALL-UNNAMED" ^
     --icon "%APP_ICON%" ^
     --class "vdm.Main"
